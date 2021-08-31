7c93b93258af7b36610133af09387f1047097416
LarryPriest <larrytpriest@gmail.com>
2021-08-30 22:36:45 -0600

    Added reverse laser fire

--- LaserFire.py
+++ LaserFire.py
@@ -10,9 +10,9 @@ Software:
     CircuitPython or micropython whichever I can get to work.
 '''
 import board
-import analogio
 import pwmio 
 import time
+import analogio
 from adafruit_simplemath import map_range
 
 
@@ -23,29 +23,30 @@ class LaserFire():
     TRACERS = 3  # number of tracers
     FullOnDutyCycle = 0xffff  # full on
     DutyCycle = []
-    pathlength = TOTALBITS/2
+    pathlength = int(TOTALBITS/2)
     speedcontrol = True
     new_min = 0.015  # min and max delay time(seconds)
     new_max = 2
     delaySpeed = analogio.AnalogIn(board.GP28)  # rotary for speed of bolt
+    direction = 1
 
-    
     def __init__(self, **kwargs):
         # Only allowed for tracers and path length
         if kwargs != {}:
             if 'tracers' in kwargs:
                 LaserFire.TRACERS = kwargs['tracers']
-            if 'range' in kwargs:
-                LaserFire.pathlength = kwargs['range']
+            if 'lrange' in kwargs:
+                LaserFire.pathlength = kwargs['lrange']
             if 'laserSpeed' in kwargs:  # laserSpeed in seconds
                 if kwargs['laserSpeed'] != '':
                     LaserFire.speedcontrol = False
                     LaserFire.delaySpeed = kwargs['laserSpeed']
                 else:
                     LaserFire.delaySpeed = LaserFire.delaySpeed
-        # Set the tracer PWM   
-        for i in reversed(range(LaserFire.TRACERS)):
-            LaserFire.DutyCycle.append(int(LaserFire.FullOnDutyCycle/(LaserFire.TRACERS-i)))
+            if 'direction' in kwargs:
+                LaserFire.direction = kwargs['direction']
+                
+       
         # Setup LED list
         subcommand1 = 'LaserFire.LEDbits.append(pwmio.PWMOut(board.GP'
         subcommand2 = ', frequency=1000))'
@@ -53,37 +54,71 @@ class LaserFire():
             fullcommand = subcommand1 + str(i) + subcommand2
             exec(fullcommand)
 
-    def Fire():
+    def Fire(direction=1):
+        LaserFire.direction = direction
         if LaserFire.speedcontrol:
-             # Get the delay from the rotary - may change if deeded to pass as param.
+             # Get the delay from the rotary - may change if needed to pass as param.
             remapped_delaySpeed = map_range(LaserFire.delaySpeed.value,200, 65520,
                                             LaserFire.new_min, LaserFire.new_max)
         else:
             remapped_delaySpeed = LaserFire.delaySpeed
-            
-        for i in range(LaserFire.pathlength):
-            
-            j = i + LaserFire.pathlength
-            # set bolt and tracers
-            for d in range(LaserFire.TRACERS):
-                print(remapped_delaySpeed, i, d, LaserFire.DutyCycle[d])
-                LaserFire.LEDbits[i-d].duty_cycle = LaserFire.DutyCycle[d]
-                LaserFire.LEDbits[j-d].duty_cycle = LaserFire.DutyCycle[d]
-            # cleanup overflowed tracers
-            for x in range(LaserFire.TRACERS):
-                if i < x:
-                    LaserFire.LEDbits[i-x].duty_cycle = 0
-                    LaserFire.LEDbits[j-x].duty_cycle = 0
+         # Set the tracer PWM
+        if LaserFire.direction == 1:
+            for i in reversed(range(LaserFire.TRACERS)):
+                LaserFire.DutyCycle.append(int(LaserFire.FullOnDutyCycle/(LaserFire.TRACERS-i)))
+        else:
+            for i in range(LaserFire.TRACERS):
+                LaserFire.DutyCycle.append(int(LaserFire.FullOnDutyCycle/(LaserFire.TRACERS-i)))
+        
+        if LaserFire.direction == 1:
+            for i in range(LaserFire.pathlength):
+                j = i + LaserFire.pathlength
+                # set bolt and tracers
+                for d in range(LaserFire.TRACERS):
+                    LaserFire.LEDbits[i-d].duty_cycle = LaserFire.DutyCycle[d]
+                    LaserFire.LEDbits[j-d].duty_cycle = LaserFire.DutyCycle[d]
+                # cleanup overflowed tracers
+                for x in range(LaserFire.TRACERS):
+                    if i < x:
+                        LaserFire.LEDbits[i-x].duty_cycle = 0
+                        LaserFire.LEDbits[j-x].duty_cycle = 0
 
-            time.sleep(remapped_delaySpeed)
-            
-            # Turn everthing off
-            for i in range(len(LaserFire.LEDbits)):  # turn off all LED's
-                LaserFire.LEDbits[i].duty_cycle = 0x0000
+                time.sleep(remapped_delaySpeed)
+                # Turn everthing off
+                for i in range(len(LaserFire.LEDbits)):  # turn off all LED's
+                    LaserFire.LEDbits[i].duty_cycle = 0
+        else:
+            for i in reversed(range(LaserFire.pathlength)):
+                j = LaserFire.pathlength + i
+                
+                # set bolt and tracers
+                for d in reversed(range(LaserFire.TRACERS)):
+                    print('i,j, d',i,j, d)
+                    LaserFire.LEDbits[i-d].duty_cycle = LaserFire.DutyCycle[d]
+                    LaserFire.LEDbits[j-d].duty_cycle = LaserFire.DutyCycle[d]
+                # cleanup overflowed tracers
+                for x in reversed(range(LaserFire.TRACERS)):
+                    if i > LaserFire.pathlength - x:
+                        print('x tracer overflow',i,x)
+                        LaserFire.LEDbits[i-x].duty_cycle = 0
+                        LaserFire.LEDbits[j-x].duty_cycle = 0
+                time.sleep(remapped_delaySpeed)
+                    # Turn everthing off
+                for i in range(len(LaserFire.LEDbits)):  # turn off all LED's
+                    LaserFire.LEDbits[i].duty_cycle = 0
+        
+    def LaserFireClose():
+        print('Closing outputs.')
+        for i in range(len(LaserFire.LEDbits)):
+            LaserFire.LEDbits[i].deinit()
 
+           
 
 if __name__ == '__main__':
 #     LaserFire(tracers=3, range=8, laserSpeed=0.2)  # laserSpeed is steps/sec (in sec.)
-    LaserFire(tracers=3, range=8)
-    while True:
-        LaserFire.Fire()
+    LaserFire()
+    try:
+        while True:
+            LaserFire.Fire(direction=-1)
+    except KeyboardInterrupt:
+        LaserFire.LaserFireClose()
